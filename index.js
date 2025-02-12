e1952c61d3948fd496020a39ecd80033081df42c
MoraTellez <ramsess1903@hotmail.com>
2025-02-12 17:46:40 -0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 // Endpoint para obtener la tasa de cambio
 app.get('/get-rate', async (req, res) => {
